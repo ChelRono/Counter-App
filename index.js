97e2879ffbf2c9f4581965883c7caff6dfe21d1c
Valarie <Valarierono97@gmail.com>
2024-02-15 15:59:13 +0300

    Adds increment,decrement and save functionality

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-let count = 14
+let count = 0
+let countEl = document.getElementById("start")
+let saveEl = document.getElementById("save-el")
+console.log(saveEl)
 
-console.log(count)
+function increment() {
+    count += 1
+    countEl.textContent = count
+    
+}
+function save() {
+    let countDash = count + "-"
+    saveEl.textContent += countDash
+    countEl.textContent = 0
+    
+}
+function decrement() {
+    count -= 1
+    countEl.innerText = count
+    
+}
